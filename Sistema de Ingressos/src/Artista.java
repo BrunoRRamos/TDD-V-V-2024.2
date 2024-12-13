41eff2e0c41b3540102046fa44763245af4ea312
brunorramos <bruno.ramos.ext@synchro.com.br>
2024-12-12 22:17:01 -0300

    TDD: Implementação do relatório

---
+++ Sistema de Ingressos/src/Artista.java
@@ -0,0 +1,13 @@
+public class Artista {
+    private String nome;
+    private double cache;
+
+    public Artista(String nome, double cache) {
+        this.nome = nome;
+        this.cache = cache;
+    }
+
+    public double getCache() {
+        return cache;
+    }
+}
